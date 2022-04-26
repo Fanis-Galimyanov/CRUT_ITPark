ff2deb77f17e74b5276b1fb39eba9c47aecaf90f
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-04-26 21:35:21 +0300

    Add project files.

---
+++ CRUT/Data/CRUTContext.cs
@@ -0,0 +1,16 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+using CRUT.Models;
+
+namespace CRUT.Data
+{
+    public class CRUTContext : DbContext
+    {
+        public CRUTContext (DbContextOptions<CRUTContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coin> Coin { get; set; }
+    }
+}
---
+++ CRUT/Models/SetData.cs
@@ -0,0 +1,48 @@
+﻿using CRUT.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUT.Models
+{
+    public class SetData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new CRUTContext(
+            serviceProvider.GetRequiredService<
+            DbContextOptions<CRUTContext>>()))
+            {
+                if (context == null || context.Coin == null)
+                {
+                    throw new ArgumentNullException("Null CryptoDemoContext");
+                }
+                if (context.Coin.Any())
+                {
+                    return;
+                }
+                context.Coin.AddRange(
+                new Coin
+                {
+                Name = "Bitcoin",
+                Price = 42920
+                },
+                new Coin
+                {
+                 Name = "Litecoin",
+                Price = 570
+                },
+                new Coin
+                {
+                 Name = "Dogecoin",
+                 Price = 23
+                },
+                new Coin
+                   {
+                Name = "BNB",
+                Price = 450
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
